59a74ab22ddb4eb8a484270b627e560f48e880c0
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2023-12-25 08:25:44 +0530

    Common Utilitiest and Browser Invocation Through Fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,17 @@
 import pytest
-from  selenium import webdriver
+from selenium import webdriver
 import time
 
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-
-
 @pytest.fixture(scope="class")
 def setup(request):
     service_obj = Service("C:\Program Files (x86)\chromedriver.exe")
     driver = webdriver.Chrome(service=service_obj)
     driver.maximize_window()
+    driver.implicitly_wait(4)
     driver.get("https://rahulshettyacademy.com/angularpractice/")
+    print(driver.title)
+    print(driver.current_url)
     request.cls.driver = driver
     yield
     driver.close()
--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,18 +1,58 @@
-import pytest
-from  selenium import webdriver
-import time
 
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 from utilities.BaseClass import BaseClass
 
-
-# @pytest.mark.usefixtures("setup")
 class TestOne(BaseClass):
     def test_e2e(self):
         self.driver.find_element(By.CSS_SELECTOR,"a[href*='shop']").click()
-        cards = self.driver.find_element(By.CSS_SELECTOR,"")
+        cards = self.driver.find_elements(By.XPATH,"//div[@class='card h-100']")
+        for card in cards:
+            productName = card.find_element(By.XPATH, "div/h4/a").text
+            if productName == "Blackberry":
+                card.find_element(By.XPATH, "div/button").click()
+        self.driver.find_element(By.CSS_SELECTOR, "a[class*='btn-primary']").click()
+        self.driver.find_element(By.XPATH, "//button[@class='btn btn-success']").click()
+        self.driver.find_element(By.CSS_SELECTOR, "input[class*='validate']").send_keys("ind")
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(expected_conditions.presence_of_element_located((By.LINK_TEXT, "India")))
+        self.driver.find_element(By.LINK_TEXT, "India").click()
+        self.driver.find_element(By.XPATH, "//div[@class='checkbox checkbox-primary']").click()
+        self.driver.find_element(By.CSS_SELECTOR, "input[type='submit']").click()
+        successText = self.driver.find_element(By.CSS_SELECTOR, "div[class*='alert-success']").text
+        print(successText)
+        assert "Success! Thank you!" in successText
+
+        # Regular Expression XPATH--partial--<a class="nav-link" href="/angularpractice/shop" xpath="1">Shop</a>
+        # Regual Expression CSS_SELECTOR   a[href*='shop']
+        # driver.find_element(By.XPATH,"//a[contains(@href, 'shop')]").click()
+        # driver.find_element(By.CSS_SELECTOR, "a[href*='shop']").click()
+        # //div[@class='card h-100']/div/h4/a
+
+
+
+# C:\Users\hp\PycharmProjects\PythonSelfFramework>pytest -v -s
+# ========================================================================= test session starts =========================================================================
+# platform win32 -- Python 3.8.2, pytest-7.4.3, pluggy-1.3.0 -- c:\python38\python.exe
+# cachedir: .pytest_cache
+# metadata: {'Python': '3.8.2', 'Platform': 'Windows-10-10.0.19041-SP0', 'Packages': {'pytest': '7.4.3', 'pluggy': '1.3.0'}, 'Plugins': {'html': '4.1.1', 'metadata': '3.0.0', 'xdist': '3.5.0'}}
+# rootdir: C:\Users\hp\PycharmProjects\PythonSelfFramework
+# plugins: html-4.1.1, metadata-3.0.0, xdist-3.5.0
+# collected 1 item
+#
+# tests/test_e2e.py::TestOne::test_e2e
+# DevTools listening on ws://127.0.0.1:52800/devtools/browser/afeed4fb-a19c-4278-b6a1-48388cdfd1f6
+# ProtoCommerce
+# https://rahulshettyacademy.com/angularpractice/
+# ×
+# Success! Thank you! Your order will be delivered in next few weeks :-).
+# PASSED
+#
+# ========================================================================= 1 passed in 11.76s ==========================================================================
+
+
+
 
 
 
